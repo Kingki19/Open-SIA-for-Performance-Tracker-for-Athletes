50c4b4a5c826030706f632ff35ce24604e1a9d5f
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-02-25 01:01:08 +0700

    Update 1_🏡_homepage.py
    tes

--- 1_🏡_homepage.py
+++ 1_🏡_homepage.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
-st.title('Open SIA Performance Tracker for Athletes')
-st.write('it just a prototype')
+st.title('Open SIA (System Information Apps) Performance Tracker for Athletes')
+
+data_profil_col, input_col, gambar_lokasi_sentra_col = st.columns(3)
+fasilitas_col, preview_kegiatan_col, monitoring_evaluation_col = st.columns(3)
+
+with data_profil_col:
+  with st.container(border=True):
+    st.image('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+    st.write('Tekan ini untuk masuk ke data profil')
+
+st.caption('it just a prototype, made to finish final project of software development course')
