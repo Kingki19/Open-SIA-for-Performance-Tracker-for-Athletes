05c50d0e26e8b507994249bad9bbeb61e21cd869
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-02-25 01:38:00 +0700

    Update 1_🏡_homepage.py
    tes, menambahkan column dalam homepage

--- 1_🏡_homepage.py
+++ 1_🏡_homepage.py
@@ -5,9 +5,23 @@ st.title('Open SIA (System Information Apps) Performance Tracker for Athletes')
 data_profil_col, input_col, gambar_lokasi_sentra_col = st.columns(3)
 fasilitas_col, preview_kegiatan_col, monitoring_evaluation_col = st.columns(3)
 
-with data_profil_col:
-  with st.container(border=True):
-    st.image('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
-    st.write('Tekan ini untuk masuk ke data profil')
+def create_container_in_column(col, image_link, label_button):
+  with col:
+    with st.container(border=True):
+      st.image(image_link)
+      st.button(label=label_button, use_container_width=True)
+
+# with data_profil_col:
+#   with st.container(border=True):
+#     st.image('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+#     st.write('Tekan ini untuk masuk ke data profil')
+
+create_container_in_column(data_profil_col, 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 'data_profil')
+create_container_in_column(input_col, 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 'input')
+create_container_in_column(gambar_lokasi_sentra_col, 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 'gambar_lokasi_sentra')
+create_container_in_column(fasilitas_col, 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 'fasilitas')
+create_container_in_column(preview_kegiatan_col, 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 'preview_kegiatan')
+create_container_in_column(monitoring_evaluation_col, 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 'monitoring_evaluation')
+
 
 st.caption('it just a prototype, made to finish final project of software development course')
