1e5b41933770464495326a9844e2e15d39d104c0
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-02-25 19:23:54 +0700

    Update 1_🏡_homepage.py

--- 1_🏡_homepage.py
+++ 1_🏡_homepage.py
@@ -21,12 +21,12 @@ def create_container_in_column(col, label_button, label_explanation):
         st.write(label_explanation)
         
     
-create_container_in_column(profiles_data_col, '👤 profiles data', 'Contain profiles data from athletes, coaches, and others')
-create_container_in_column(input__data_col, '📥 input data', 'Input athlete performance data every day here')
-create_container_in_column(image_center_location_col, '🗺️ image of center location', 'I don\'t know what is this')
-create_container_in_column(facilities_col, '👟 facilities', 'facilities provided by the relevant training site (university)')
-create_container_in_column(activities_preview_col, '🏃 activities preview', 'Plan of activities to be implemented')
-create_container_in_column(monitoring_evaluation_col, '📊 monitoring evaluation', 'Monitor athletes\'s performance based on their daily data and competition results')
+create_container_in_column(profiles_data_col, 'profiles data', 'Contain profiles data from athletes, coaches, and others')
+create_container_in_column(input__data_col, 'input data', 'Input athlete performance data every day here')
+create_container_in_column(image_center_location_col, 'image of center location', 'I don\'t know what is this')
+create_container_in_column(facilities_col, 'facilities', 'facilities provided by the relevant training site (university)')
+create_container_in_column(activities_preview_col, 'activities preview', 'Plan of activities to be implemented')
+create_container_in_column(monitoring_evaluation_col, 'monitoring evaluation', 'Monitor athletes\'s performance based on their daily data and competition results')
 
 
 st.caption('it just a prototype, made to finish final project of software development course')
