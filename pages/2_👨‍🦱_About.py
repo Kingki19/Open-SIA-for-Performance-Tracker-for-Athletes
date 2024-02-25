3478cfb2574ddfce846a21425e0dc32136c0e51e
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-02-25 19:55:58 +0700

    Update 2_👨‍🦱_About.py
    add contributors

--- pages/2_👨‍🦱_About.py
+++ pages/2_👨‍🦱_About.py
@@ -1,17 +1,24 @@
 import streamlit as st
 
+st.header('Our core team', divider=True)
 our_team = """
 - Muhammad Rizqi = Project Manager, Project Initiator (who manage and own apps)
 - Raihan Muhammad Rif'at = Database Engineer (who create and handle database, we still search free alternative)
 - Nuzulul Barkah Rifai = System Analyst (who create how and why each element works, new feature it's responsible)
 - Muhammad Habid Ali = Fullstack Dev (who help me code and create UI)
 """
-reason = '''
-We made this app to finish our last project on software development and help ms. Wiga to make app for athletes.
-'''
-
-st.header('Our team', divider=True)
 st.markdown(our_team)
 
 st.header('Reason', divider=True)
+reason = '''
+We made this app to finish our last project on software development and help ms. Wiga to make app for athletes.
+'''
 st.markdown(reason)
+
+st.header('Contributors', divider=True)
+contributors_html = '''
+<a href="https://github.com/Kingki19/Open-SIA-for-Performance-Tracker-for-Athletes/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=Kingki19/Open-SIA-for-Performance-Tracker-for-Athletes" />
+</a>
+'''
+st.markdown(contributors_html, unsafe_allow_html=True)
